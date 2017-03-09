b97ba005f490e3db3fda3720fe1037e02997dbac
hilgert <hilgertfacul@gmail.com>
2017-03-09 14:33:06 +0000

    Setting webhook

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const app = express();
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+bot.setWebHook('https://student-drivebot.herokuapp.com/bot${process.env.TELEGRAM_BOT_TOKEN}');
 
 bot.on('message', function(msg){
     bot.sendMessage(msg.chat.id, msg.text);
